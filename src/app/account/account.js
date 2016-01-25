da94d94db44d7b60125dd0b0734ea3a8a1b9fd3c
akram moncer <akram.moncer@piterion.com>
2016-01-25 15:08:53 +0100

    Tutorial covering ng-resource to create user accounts. https://www.youtube.com/watch?v=BU0opD_gydw

--- src/app/account/account.js
+++ src/app/account/account.js
@@ -1,4 +1,4 @@
-angular.module('ngBoilerplate.account', ['ui.router'])
+angular.module('ngBoilerplate.account', ['ui.router', 'ngResource'])
 .config(function($stateProvider){
 	$stateProvider.state('login', {
 		url:'/login',
@@ -24,6 +24,7 @@ angular.module('ngBoilerplate.account', ['ui.router'])
 .factory('sessionService', function(){
 	var session = {};
 	session.login = function(data){
+		alert("user loggedIn " + data.name + " and " + data.password);
 		localStorage.setItem("session", data);
 	};
 	session.logout = function(){
@@ -34,15 +35,45 @@ angular.module('ngBoilerplate.account', ['ui.router'])
 	};
 	return session;
 })
-.controller('LoginCtrl',function($scope, sessionService, $state){
+.factory('accountService', function($resource){
+	var service = {};
+	service.register = function(account, success, failure){
+		var Account = $resource("http://localhost:8080/spring_angularjs/rest/accounts");
+		Account.save({}, account, success, failure);
+	};
+	service.userExists = function(account, success, failure){
+		var Account = $resource("http://localhost:8080/spring_angularjs/rest/accounts");
+		var data = Account.get({name:account.name}, function(){
+			var accounts = data.accounts;
+			if(accounts.length !== 0){
+				success(accounts[0]);
+			}else{
+				failure();
+			}
+		},failure);
+	};
+	return service;
+})
+.controller('LoginCtrl',function($scope, sessionService, accountService, $state){
 	$scope.login = function(){
-		sessionService.login($scope.account);
-		$state.go('home');
+		accountService.userExists($scope.account, function(account){
+			sessionService.login($scope.account);
+			$state.go('home');
+		},
+		function(){
+			alert("Error");
+		});
 	};
 })
-.controller('RegisterCtrl',function($scope, sessionService, $state){
+.controller('RegisterCtrl',function($scope, sessionService, $state, accountService){
 	$scope.register = function(){
-		sessionService.login($scope.account);
-		$state.go('home');
+		accountService.register($scope.account, function(returnedData){
+			sessionService.login(returnedData);
+			$state.go('home');
+		},
+		function(){
+			alert("fail to register");
+		});
+		
 	};
 });
